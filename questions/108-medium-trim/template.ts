bc57852695e8a823eea8e7686b9c4b768c179d86
gustavo <gustavosalazar@ravn.co>
2025-04-17 15:29:28 -0500

    feat(108-medium-trim):Completed

--- questions/108-medium-trim/template.ts
+++ questions/108-medium-trim/template.ts
@@ -1,1 +1,7 @@
-export type Trim<S extends string> = any
+type Not = " " | "\n" | "\t"
+
+export type Trim<S extends string> =
+  S extends `${Not}${infer R}${Not}` ? Trim<R> :
+  S extends `${Not}${infer R}` ? Trim<R> :
+  S extends `${infer R}${Not}` ? Trim<R> :
+  S
