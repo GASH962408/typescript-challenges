f74e6c658eacc2eaa571f39d9aabb7aeb88bfae7
gustavo <gustavosalazar@ravn.co>
2025-04-18 10:09:16 -0500

    feat(529-medium-absolute):Completed

--- questions/529-medium-absolute/template.ts
+++ questions/529-medium-absolute/template.ts
@@ -1,1 +1,11 @@
-export type Absolute<T extends number | string | bigint> = any
+export type Absolute<T extends number | string | bigint> = 
+T extends string
+    ? T extends `-${infer R}` 
+      ? R 
+      : T
+    : T extends number | bigint
+      ? `${T}` extends `-${infer R}` 
+        ? R 
+        : `${T}`
+      : never
+
