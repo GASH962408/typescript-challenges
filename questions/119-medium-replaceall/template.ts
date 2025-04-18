26d2a38ec2c21ceb7ac36935aad74b30c9800e79
gustavo <gustavosalazar@ravn.co>
2025-04-17 20:50:47 -0500

    feat(119-medium-replaceall):Complete

--- questions/119-medium-replaceall/template.ts
+++ questions/119-medium-replaceall/template.ts
@@ -2,4 +2,14 @@ export type ReplaceAll<
   S extends string,
   From extends string,
   To extends string
-> = any
+> =
+  From extends ""
+    ? S
+    : From extends To
+      ? S
+      : S extends `${infer Before}${From}${infer After}`
+        ? `${Before}${To}${ReplaceAll<After,From,To>}`
+        : S;
+
+        type example = 'foobarfoobar'
+        type newResult = ReplaceAll<example, "ob", "b"> 
